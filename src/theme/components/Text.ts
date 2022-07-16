a2da56fe033e2ead16651258e5dc48d4f51631c6
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-07-16 22:51:28 +0700

    Override `Text`

---
+++ src/theme/components/Text.ts
@@ -0,0 +1,20 @@
+import { ComponentStyleConfig } from "@chakra-ui/react";
+
+export enum TextVariants {
+
+}
+export enum TextSizes {
+
+}
+
+const Text: ComponentStyleConfig = {
+	baseStyle : {
+		fontSize   : "xl",
+		color      : "not-quite-black.0",
+		fontWeight : 400,
+		fontStyle  : "normal",
+		lineHeight : "tall"
+	},
+};
+
+export default Text;
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,6 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 import colors from "./colors";
 import Button from "./components/Button";
 import Heading from "./components/Heading";
+import Text from "./components/Text";
 import fonts from "./fonts";
 import gradients from "./gradients";
 import shadows from "./shadows";
@@ -17,7 +18,8 @@ const theme = extendTheme({
 	spacing,
 	components : {
 		Button,
-		Heading
+		Heading,
+		Text
 	},
 });
 
