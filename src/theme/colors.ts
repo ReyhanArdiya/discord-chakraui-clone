e8288d423f280aa5f0118e1364d021e81c91e3de
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-07-16 19:50:18 +0700

    Add colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,18 @@
+const colors = {
+	brand                 : "#5865f2",
+	greyple               : "#99aab5",
+	"dark-not-black"      : "#2c2f33",
+	"focus-border"        : "#00b0f4",
+	"status-green"        : "#43b581",
+	"text-link"           : "#00b0f4",
+	"off-white"           : "#f6f6f6",
+	"not-quite-black"     : "#23272a",
+	"not-quite-black-rgb" : "35,39,42",
+	"ekko-red"            : "#de2761",
+
+	// Already available in chakra
+	// white                 : "#fff",
+	// black                 : "#000",
+};
+
+export default colors;
