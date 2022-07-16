0d3e3998842807016b77b21b4d266ff2526f62cd
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-07-16 21:59:46 +0700

    Implement `Button` overrides

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,7 +1,9 @@
 import { extendTheme } from "@chakra-ui/react";
 import colors from "./colors";
+import Button from "./components/Button";
 import fonts from "./fonts";
 import gradients from "./gradients";
+import shadows from "./shadows";
 import sizes from "./sizes";
 import spacing from "./spacing";
 
@@ -9,8 +11,12 @@ const theme = extendTheme({
 	colors,
 	fonts,
 	gradients,
+	shadows,
 	sizes,
 	spacing,
+	components : {
+		Button,
+	},
 });
 
 export default theme;
