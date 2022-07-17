e5813a20efa12394471683ac714efe3b48b01677
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-07-17 11:09:08 +0700

    Add discord tab icon

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+	return (
+		<Html>
+			<Head>
+				<link href="/images/847541504914fd33810e70a0ea73177e.ico" rel="icon"/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
