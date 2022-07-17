837a5fe686da4c6677b2349a34d1a2cbf5c245f0
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-07-17 11:09:20 +0700

    Give head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import GlobalLayout from "../components/Layouts/GlobalLayout";
 import theme from "../theme";
 import Fonts from "../theme/Fonts";
@@ -8,6 +9,13 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 	return (
 		<ChakraProvider theme={theme}>
 			<Fonts />
+			<Head>
+				<meta
+					content="width=device-width, initial-scale=1.0"
+					name="viewport"
+				/>
+				<title>Discord | Clone</title>
+			</Head>
 			<GlobalLayout>
 				<Component {...pageProps} />
 			</GlobalLayout>
